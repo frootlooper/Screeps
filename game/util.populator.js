f9994620db402741252333b77a81923bac57f8ef
frootlooper <gwen.macnichol@gmail.com>
2016-11-16 16:31:39 -0500

    Increased harvester and upgrader work as well

--- game/util.populator.js
+++ game/util.populator.js
@@ -13,14 +13,14 @@ var utilPopulator = {
 
         console.log('Harvesters: ' + harvesters.length + ', Builders: ' + builders.length + ', Upgraders: ' + upgraders.length);
 
-        if(harvesters.length < 3 && Game.spawns['Spawn1'].room.energyAvailable >= 300) {
-            var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+        if(harvesters.length < 3 && Game.spawns['Spawn1'].room.energyAvailable >= 400) {
+            var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,CARRY,MOVE], undefined, {role: 'harvester'});
             console.log('Spawning new harvester: ' + newName);
         } else if(builders.length < 3 && Game.spawns['Spawn1'].room.energyAvailable >= 400) {
             var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,CARRY,MOVE], undefined, {role: 'builder'});
             console.log('Spawning new builder: ' + newName);
-        } else if(upgraders.length < 3 && Game.spawns['Spawn1'].room.energyAvailable >= 300) {
-            var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
+        } else if(upgraders.length < 3 && Game.spawns['Spawn1'].room.energyAvailable >= 400) {
+            var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
             console.log('Spawning new upgrader: ' + newName);
         }
     }
