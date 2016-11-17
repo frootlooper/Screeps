a7892e8b1350799ceea7c03beedbe8184f3f98d5
frootlooper <gwen.macnichol@gmail.com>
2016-11-17 18:34:06 -0500

    Auto increases work parts for creeps

--- game/util.populator.js
+++ game/util.populator.js
@@ -13,26 +13,36 @@ var utilPopulator = {
 
         console.log('Harvesters: ' + harvesters.length + ', Builders: ' + builders.length + ', Upgraders: ' + upgraders.length);
 
-        //TODO: calculated constants to determine population needed
-        var totalPossibleEnergy = Game.spawns['Spawn1'].room.energyCapacity;
-        var numConstructionSites = Game.constructionSites.length;
+        var totalRoomCapacity = Game.spawns['Spawn1'].room.energyCapacityAvailable;
+        var totalRoomAvailable = Game.spawns['Spawn1'].room.energyAvailable;
         var basicBodyParts = [MOVE,CARRY,WORK];
-        var basicBodyPartsEnergyCost = 200;
 
-        if(harvesters.length < 3 && Game.spawns['Spawn1'].room.energyAvailable == totalPossibleEnergy) {
-          var harvesterBodyParts = basicBodyParts.slice();
-          var harvesterBodyPartsEnergyCost = 200;
-          while (harvesterBodyPartsEnergyCost < totalPossibleEnergy + BODYPART_COST["work"]) {
-            harvesterBodyParts.push(WORK);
-            harvesterBodyPartsEnergyCost = harvesterBodyPartsEnergyCost + BODYPART_COST["work"];
-          }
-          var newName = Game.spawns['Spawn1'].createCreep(bodyParts, undefined, {role: 'harvester'});
-          console.log('Spawning new harvester: ' + newName);
-        } else if(upgraders.length < 3 && Game.spawns['Spawn1'].room.energyAvailable == totalPossibleEnergy) {
-            var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,WORK,WORK,WORK,WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
+        if (harvesters.length < 3 && totalRoomAvailable == totalRoomCapacity) {
+            var harvesterBodyParts = basicBodyParts.slice();
+            var harvesterBodyPartsEnergyCost = BODYPART_COST["move"] + BODYPART_COST["carry"] + BODYPART_COST["WORK"];
+            while (harvesterBodyPartsEnergyCost <= totalRoomAvailable + BODYPART_COST["work"]) {
+                harvesterBodyParts.push(WORK);
+                harvesterBodyPartsEnergyCost = harvesterBodyPartsEnergyCost + BODYPART_COST["work"];
+            }
+            var newName = Game.spawns['Spawn1'].createCreep(harvesterBodyParts, undefined, {role: 'harvester'});
+            console.log('Spawning new harvester: ' + newName);
+        } else if (upgraders.length < 3 && totalRoomAvailable == totalRoomCapacity) {
+            var upgraderBodyParts = basicBodyParts.slice();
+            var upgraderBodyPartsEnergyCost = BODYPART_COST["move"] + BODYPART_COST["carry"] + BODYPART_COST["WORK"];
+            while (upgraderBodyPartsEnergyCost <= totalRoomAvailable + BODYPART_COST["work"]) {
+                upgraderBodyParts.push(WORK);
+                upgraderBodyPartsEnergyCost = upgraderBodyPartsEnergyCost + BODYPART_COST["work"];
+            }
+            var newName = Game.spawns['Spawn1'].createCreep(upgraderBodyParts, undefined, {role: 'upgrader'});
             console.log('Spawning new upgrader: ' + newName);
-        } else if(builders.length < 10 && Game.spawns['Spawn1'].room.energyAvailable == totalPossibleEnergy) {
-            var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,CARRY,MOVE], undefined, {role: 'builder'});
+        } else if (builders.length < 10 && totalRoomAvailable == totalRoomCapacity) {
+            var builderBodyParts = basicBodyParts.slice();
+            var builderBodyPartsEnergyCost = BODYPART_COST["move"] + BODYPART_COST["carry"] + BODYPART_COST["WORK"];
+            while (builderBodyPartsEnergyCost <= totalRoomAvailable + BODYPART_COST["work"]) {
+                builderBodyParts.push(WORK);
+                builderBodyPartsEnergyCost = builderBodyPartsEnergyCost + BODYPART_COST["work"];
+            }
+            var newName = Game.spawns['Spawn1'].createCreep(builderBodyParts, undefined, {role: 'builder'});
             console.log('Spawning new builder: ' + newName);
         }
     }
