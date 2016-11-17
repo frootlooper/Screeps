57394a83e2d9d4d577a89faddce8e7bcc07609ee
frootlooper <gwen.macnichol@gmail.com>
2016-11-17 18:28:46 -0500

    Populator automatically increases work parts based on energy availability

--- game/util.populator.js
+++ game/util.populator.js
@@ -16,14 +16,22 @@ var utilPopulator = {
         //TODO: calculated constants to determine population needed
         var totalPossibleEnergy = Game.spawns['Spawn1'].room.energyCapacity;
         var numConstructionSites = Game.constructionSites.length;
+        var basicBodyParts = [MOVE,CARRY,WORK];
+        var basicBodyPartsEnergyCost = 200;
 
-        if(harvesters.length < 3 && Game.spawns['Spawn1'].room.energyAvailable >= 800) {
-            var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,MOVE], undefined, {role: 'harvester'});
-            console.log('Spawning new harvester: ' + newName);
-        } else if(upgraders.length < 3 && Game.spawns['Spawn1'].room.energyAvailable >= 800) {
+        if(harvesters.length < 3 && Game.spawns['Spawn1'].room.energyAvailable == totalPossibleEnergy) {
+          var harvesterBodyParts = basicBodyParts.slice();
+          var harvesterBodyPartsEnergyCost = 200;
+          while (harvesterBodyPartsEnergyCost < totalPossibleEnergy + BODYPART_COST["work"]) {
+            harvesterBodyParts.push(WORK);
+            harvesterBodyPartsEnergyCost = harvesterBodyPartsEnergyCost + BODYPART_COST["work"];
+          }
+          var newName = Game.spawns['Spawn1'].createCreep(bodyParts, undefined, {role: 'harvester'});
+          console.log('Spawning new harvester: ' + newName);
+        } else if(upgraders.length < 3 && Game.spawns['Spawn1'].room.energyAvailable == totalPossibleEnergy) {
             var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,WORK,WORK,WORK,WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
             console.log('Spawning new upgrader: ' + newName);
-        } else if(builders.length < 10 && Game.spawns['Spawn1'].room.energyAvailable >= 800) {
+        } else if(builders.length < 10 && Game.spawns['Spawn1'].room.energyAvailable == totalPossibleEnergy) {
             var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,CARRY,MOVE], undefined, {role: 'builder'});
             console.log('Spawning new builder: ' + newName);
         }
