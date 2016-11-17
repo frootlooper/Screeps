021b6fa33b2f8feb049c113fc36765f309b2f1bf
Gwen Macnichol <gmacnichol@impactradius.com>
2016-11-17 11:42:24 -0500

    Updated to 1000 energy unit creep creation

--- game/util.populator.js
+++ game/util.populator.js
@@ -13,14 +13,14 @@ var utilPopulator = {
 
         console.log('Harvesters: ' + harvesters.length + ', Builders: ' + builders.length + ', Upgraders: ' + upgraders.length);
 
-        if(harvesters.length < 3 && Game.spawns['Spawn1'].room.energyAvailable >= 550) {
-            var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,WORK,CARRY,CARRY,MOVE], undefined, {role: 'harvester'});
+        if(harvesters.length < 3 && Game.spawns['Spawn1'].room.energyAvailable >= 1000) {
+            var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,MOVE], undefined, {role: 'harvester'});
             console.log('Spawning new harvester: ' + newName);
-        } else if(upgraders.length < 3 && Game.spawns['Spawn1'].room.energyAvailable >= 550) {
-            var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,WORK,CARRY,CARRY,MOVE], undefined, {role: 'upgrader'});
+        } else if(upgraders.length < 3 && Game.spawns['Spawn1'].room.energyAvailable >= 1000) {
+            var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,WORK,WORK,WORK,WORK,WORK,WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
             console.log('Spawning new upgrader: ' + newName);
-        } else if(builders.length < 10 && Game.spawns['Spawn1'].room.energyAvailable >= 550) {
-            var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,WORK,CARRY,CARRY,MOVE], undefined, {role: 'builder'});
+        } else if(builders.length < 10 && Game.spawns['Spawn1'].room.energyAvailable >= 1000) {
+            var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,WORK,WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,CARRY,MOVE], undefined, {role: 'builder'});
             console.log('Spawning new builder: ' + newName);
         }
     }
